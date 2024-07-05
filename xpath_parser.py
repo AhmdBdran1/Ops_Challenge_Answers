8ac94d6b9cfcc126bb565892d05f36a9bf3e4aa5
AhmdBdran1 <ahmdbdran@h-Air-sl-user.Dlink>
2024-07-05 17:01:09 +0300

    adjust keys name

--- xpath_parser.py
+++ xpath_parser.py
@@ -5,25 +5,23 @@ from lxml import html
 url = "https://support.mozilla.org/en-US/questions/firefox?show=all"
 response = requests.get(url)
 tree = html.fromstring(response.content)
-
 # Define XPaths
 xpaths = {
-    "The box ‘Get Help’ from the top bar": ".//a[@class='mzp-c-menu-title sumo-nav--link'][text()='Get Help']",
-    "The search box input on the top right of the page": ".//form[@id='support-search-sidebar']/input",
-    "The first post on the page": ".//article[@class='forum--question-item'][1]",
-    "The first 5 posts at the page": ".//article[@class='forum--question-item'][position()<=5]",
-    "The time of publishing of the first post": ".//article[@class='forum--question-item'][1]//p[@class='user-meta-asked-by']/text()[contains(., 'ago')]",
-    "all posts that are responded": ".//article[@class='forum--question-item' and .//li[contains(@class, 'thread-solved')]]",
-    "all posts that are “1 day ago": ".//article[.//p[@class='user-meta-asked-by']/text()[contains(.,'1 day ago')]]",
-    "All posts that '?' in the title": ".//article[.//h2[@class='forum--question-item-heading']/a[contains(text(),'?')]]",
-    "All posts that their author name starts with D (D or d)": ".//article[.//p[@class='user-meta-asked-by' and (starts-with(translate(.//strong/a/text(),'d','D'), 'D') )]]",
-    #"All posts that they have more then 2 replies": ".//article[.//dl[@class='forum--meta-details replies']//span[@class='forum--meta-val']/text() > 2]",
-    "All posts that their title starts with ‘P’ (or p)": ".//article[.//h2[@class='forum--question-item-heading' and .//a[starts-with(translate(text(), 'p', 'P'), 'P')]]]",
-    "All posts that their title length is 14 or bigger": ".//article[.//h2[@class='forum--question-item-heading' and .//a[string-length(text())>=14]]]",
-    "All the posts that has “Windows 11” tag": ".//article[.//ul[@class='tag-list push-right']/li/a[text()='Windows 11']]",
+    "get_help_box_xpath": ".//a[@class='mzp-c-menu-title sumo-nav--link'][text()='Get Help']",
+    "search_box_input_xpath": ".//form[@id='support-search-sidebar']/input",
+    "first_post_page_xpath": ".//article[@class='forum--question-item'][1]",
+    "first_5_pages_xpath": ".//article[@class='forum--question-item'][position()<=5]",
+    "time_of_publishing_first_post_xpath": ".//article[@class='forum--question-item'][1]//p[@class='user-meta-asked-by']/text()[contains(., 'ago')]",
+    "posts_that_responded_xpath": ".//article[@class='forum--question-item' and .//li[contains(@class, 'thread-solved')]]",
+    "one_day_ago_posts_xpath": ".//article[.//p[@class='user-meta-asked-by']/text()[contains(.,'1 day ago')]]",
+    "posts_contain_question_marx_xpath": ".//article[.//h2[@class='forum--question-item-heading']/a[contains(text(),'?')]]",
+    "posts_author_name_start_with_d_xpath": ".//article[.//p[@class='user-meta-asked-by' and (starts-with(translate(.//strong/a/text(),'d','D'), 'D') )]]",
+    # "posts_with_more_than_tow_replies_xpath": ".//article[.//dl[@class='forum--meta-details replies']//span[@class='forum--meta-val']/text() > 2]",
+    "posts_with_title_start_with_p_xpath": ".//article[.//h2[@class='forum--question-item-heading' and .//a[starts-with(translate(text(), 'p', 'P'), 'P')]]]",
+    "posts_with_specific_title_length_xpath": ".//article[.//h2[@class='forum--question-item-heading' and .//a[string-length(text())>=14]]]",
+    "posts_contain_Windows11_tag_xpath": ".//article[.//ul[@class='tag-list push-right']/li/a[text()='Windows 11']]",
 
 }
-
 
 # Function to apply XPath and return the number of elements founded
 def count_elements(xpath):
